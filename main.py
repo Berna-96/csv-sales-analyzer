2e10c9574831aa05bb2b38f242fce9acf6b56f2f
Berna-96 <matteobernardini8796@gmail.com>
2026-02-10 18:33:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,6 @@ from utils import average_price
 
 import csv
 
-def total_revenue(prices, quantities):
-    total = 0
-    for price, quantity in zip(prices, quantities):
-        total += price * quantity
-    return total
-
-
-def best_product(products, prices, quantities):
-    revenues = []
-    for price, quantity in zip(prices, quantities):
-        revenues.append(price * quantity)
-
-    max_index = revenues.index(max(revenues))
-    return products[max_index]
-
-def total_items_sold(quantities):
-    return sum(quantities)
-
 products = []
 prices = []
 quantities = []
