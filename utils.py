7a0878eb7701b7d7d60030abb44c2a8a5f4ed136
Berna-96 <matteobernardini8796@gmail.com>
2026-02-10 18:33:48 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,3 +2,21 @@ import csv
 
 def average_price(prices):
     return sum(prices) / len(prices)
+
+def total_revenue(prices, quantities):
+    total = 0
+    for price, quantity in zip(prices, quantities):
+        total += price * quantity
+    return total
+
+
+def best_product(products, prices, quantities):
+    revenues = []
+    for price, quantity in zip(prices, quantities):
+        revenues.append(price * quantity)
+
+    max_index = revenues.index(max(revenues))
+    return products[max_index]
+
+def total_items_sold(quantities):
+    return sum(quantities)
