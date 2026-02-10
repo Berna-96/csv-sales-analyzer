ba7e161b7a86d2fe39e1901a35c4830bfbf86a37
Berna-96 <matteobernardini8796@gmail.com>
2026-02-10 16:35:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import csv
 
 def total_revenue(prices, quantities):
@@ -39,3 +40,12 @@ print("Total revenue:", total_revenue(prices, quantities))
 print("Average price:", average_price(prices))
 print("Best product:", best_product(products, prices, quantities))
 print("Total items sold:", total_items_sold(quantities))
+
+with open("data/report.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+
+    writer.writerow(["metric", "value"])
+    writer.writerow(["total_revenue", total_revenue(prices, quantities)])
+    writer.writerow(["average_price", average_price(prices)])
+    writer.writerow(["best_product", best_product(products, prices, quantities)])
+    writer.writerow(["total_items_sold", total_items_sold(quantities)])
